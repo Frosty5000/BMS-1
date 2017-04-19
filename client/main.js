a1a232dbe5aa06680a2d9165b4f6e49cd7d79cf3
PanzarTank <paulv.wow@gmail.com>
2017-04-19 08:34:32 -0400

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,16 +1,19 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { ReactiveDict } from 'meteor/reactive-dict';
+import { Mongo } from 'meteor/mongo';
 
 import './main.html';
 
+roomNum = "0";
+
 if (Meteor.isClient) {
-    Tracker.autorun(function() {
+    Tracker.autorun(function () {
         Meteor.subscribe("allUsers");
         Meteor.subscribe("allUserData");
     });
     Template.signup.events({
-        'submit form': function(event) {
+        'submit form': function (event) {
             event.preventDefault();
             var emailVar = event.target.signupEmail.value;
             var passwordVar = event.target.signupPassword.value;
@@ -24,10 +27,12 @@ if (Meteor.isClient) {
                         name: nameVar,
                         role: "Admin",
                         createdAt: date,
-                        room_type: "0",
-                        room_number: "0",
-                        person_number: "0",
-                        child_number: "0",
+                        room_type: "",
+                        room_number: "",
+                        person_number: "",
+                        child_number: "",
+                        inDate: "",
+                        outDate: "",
                         checked_In: false
                     }
                 });
@@ -39,10 +44,12 @@ if (Meteor.isClient) {
                         name: nameVar,
                         role: "User",
                         createdAt: date,
-                        room_type: "0",
-                        room_number: "0",
-                        person_number: "0",
-                        child_number: "0",
+                        room_type: "",
+                        room_number: roomNum,
+                        person_number: "",
+                        child_number: "",
+                        inDate: "",
+                        outDate: "",
                         checked_In: false
                     }
                 });
@@ -50,7 +57,7 @@ if (Meteor.isClient) {
         }
     });
     Template.body.events({
-        'click .cancel': function(e) {
+        'click .cancel': function (e) {
             event.preventDefault();
             var date = Meteor.user().profile.createdAt;
             var name = Meteor.user().profile.name;
@@ -71,13 +78,14 @@ if (Meteor.isClient) {
             });
             //alert("Great Success!");
             $('#myModalAccount').modal('hide');
-            setTimeout(function() {
+            $('#myModalAdmin').modal('hide');
+            setTimeout(function () {
                 $('#myModal2').modal('show');
             }, 1000)
         }
     });
     Template.login.events({
-        'submit form': function(event) {
+        'submit form': function (event) {
             event.preventDefault();
             var emailVar = event.target.loginEmail.value;
             var passwordVar = event.target.loginPassword.value;
@@ -85,11 +93,11 @@ if (Meteor.isClient) {
         }
     });
     Template.settings.events({
-        'click .logout': function(event) {
+        'click .logout': function (event) {
             event.preventDefault();
             Meteor.logout();
         },
-        'submit form': function(event) {
+        'submit form': function (event) {
             event.preventDefault();
             var room_type = event.target.room_type.value;
             var room_number = event.target.room_number.value;
@@ -98,6 +106,8 @@ if (Meteor.isClient) {
             var date = Meteor.user().profile.createdAt;
             var name = Meteor.user().profile.name;
             var role = Meteor.user().profile.role;
+            inDate = event.target.inDate.value;
+            outDate = event.target.outDate.value;
             Meteor.users.update({ _id: Meteor.userId() }, {
                 "$set": {
                     "profile": {
@@ -108,6 +118,8 @@ if (Meteor.isClient) {
                         "createdAt": date,
                         "checked_In": false,
                         "name": name,
+                        "inDate": inDate,
+                        "outDate": outDate,
                         "role": role
                     }
                 }
@@ -120,68 +132,90 @@ if (Meteor.isClient) {
             event.target.child_number.value = "0";
         }
     });
+    Template.datePicker1.onRendered(function () {
+        this.$('.datetimepicker1').datetimepicker({
+            format: 'dddd, MMMM Do YYYY'
+        });
+    });
+    Template.datePicker2.onRendered(function () {
+        this.$('.datetimepicker2').datetimepicker({
+            format: 'dddd, MMMM Do YYYY'
+        });
+    });
     Template.userEmail.helpers({
-        userEmail: function() {
+        userEmail: function () {
             return Meteor.user().emails[0].address
         }
     });
+    Template.userInDate.helpers({
+        userInDate: function () {
+            return Meteor.user().profile.inDate
+        }
+    });
+    Template.userOutDate.helpers({
+        userOutDate: function () {
+            return Meteor.user().profile.outDate
+        }
+    });
     Template.userName.helpers({
-        userName: function() {
+        userName: function () {
             return Meteor.user().profile.name
         }
     });
     Template.userChecked_In.helpers({
-        userChecked_In: function() {
+        userChecked_In: function () {
             return Meteor.user().profile.checked_In
         }
     });
     Template.userRole.helpers({
-        userRole: function() {
+        userRole: function () {
             return Meteor.user().profile.role
         }
     });
     Template.userChildNumber.helpers({
-        userChildNumber: function() {
+        userChildNumber: function () {
             return Meteor.user().profile.child_number
         }
     });
     Template.userEmailVerified.helpers({
-        userEmailVerified: function() {
+        userEmailVerified: function () {
             return Meteor.user().emails[0].verified
         }
     });
     Template.userPersonNumber.helpers({
-        userPersonNumber: function() {
+        userPersonNumber: function () {
             return Meteor.user().profile.person_number
         }
     });
     Template.userCreatedAt.helpers({
-        userCreatedAt: function() {
+        userCreatedAt: function () {
             return Meteor.user().profile.createdAt
         }
     });
     Template.userNumRoom.helpers({
-        userNumRoom: function() {
+        userNumRoom: function () {
             return Meteor.user().profile.room_number
         }
     });
     Template.userRoomType.helpers({
-        userRoomType: function() {
+        userRoomType: function () {
             return Meteor.user().profile.room_type
         }
     });
     Template.daysOverview.helpers({
-        users: function() {
-            var user = Meteor.users.find({});
-            return user;
-        },
+        users() { 
+            return Meteor.users.find({});
+         },
+        email() {
+             return this.emails[0].address;
+             }
     });
     /*  REQUIRES AUTHORIZATION. POSSIBLE TO PUBLISH?
         userID: function () {
           return Meteor.user()._id
         }, */
     Template.isAdmin.helpers({
-        admin: function() {
+        admin: function () {
             if (Meteor.user().profile.role == "Admin") {
                 return true
             } else {
@@ -189,16 +223,32 @@ if (Meteor.isClient) {
             }
         }
     });
+    /*a = Meteor.users.find()
+    names = a.map((function (a) {
+        return a.names;
+    }));
+    if (!names.isArray) {
+        alert(names[0])
+    }
+    Template.userDB.helpers({
+        userD: function () {
+            return names
+        }
+    }) */
+    /*
+Please consider that the JS part isn't production ready at all, I just code it to show the concept of merging filters and titles together !
+*/
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function() {
-        // code to run on server at startup
+    Meteor.startup(function () {
     });
-    Meteor.publish('allUsers', function() {
-        return Meteor.users.find();
+    Meteor.publish('allUsers', function () {
+        return Meteor.users.find({});
     });
-    Meteor.publish('AllUserData', function() {
-        return Meteor.users.find({}, { fields: { profile: true } });
+    Meteor.publish('AllUserData', function () {
+        return Meteor.users.find({}, { fields: { emails: 1, profile: 1 } });
     });
+    Meteor.publish( 'profiles', function () { Meteor.users.find({}) });
+
 }
